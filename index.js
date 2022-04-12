6cd128308af4677080272336bd7485e46358882a
Ryan Mahoney <ryan@virtuecenter.com>
2022-04-12 13:08:49 -0400

    paginate, bracket, sort, print as tsv

--- index.js
+++ index.js
@@ -1,42 +1,54 @@
 const { Octokit } = require("@octokit/rest");
-require('dotenv').config();
+const dayjs = require("dayjs");
 
-// get token and org from local env file
+var weekOfYear = require("dayjs/plugin/weekOfYear");
+dayjs.extend(weekOfYear);
+
+require("dotenv").config();
+
+const getPRs = async (octokit, org, sinceDate) => {
+  const query = `user:${org} is:pr sort:updated-desc updated:>${sinceDate}`;
+  const items = await octokit.paginate(octokit.search.issuesAndPullRequests, {
+    q: query,
+    per_page: 100
+  });
+
+  return items.reduce((acc, pr) => {
+    const {
+      user: { login },
+      updated_at: updated
+    } = pr;
+    const [date] = updated.split("T");
+    const [year] = date.split("-");
+    const week = `${year}-${dayjs(date).week()}`;
+
+    // bracket by week
+    if (!(week in acc)) acc[week] = {};
+
+    // count by user
+    if (login in acc[week]) {
+      acc[week][login]++;
+    } else {
+      acc[week][login] = 1;
+    }
+    return acc;
+  }, {});
+};
+
+const sinceDate = `${dayjs().subtract(28, "days").format("YYYY-MM-DD")}`;
 const { token, org } = process.env;
+const oktokit = new Octokit({ auth: token });
 
-// instantiate the github Rest API clinet
-const octokit = new Octokit({ auth: token });
-
-async function paginate(method) {
-    let response = await method({
-        q: `user:${org} is:pr`,
-        per_page: 100
-    })
-    let data = response.data.items;
-    // var count = 0;
-    // octokit.
-    // while (octokit.hasNextPage(response)) {
-    //     count++;
-    //     console.log(`request n°${count}`);
-    //     response = await octokit.getNextPage(response);
-    //     data = data.concat(response.data.items);
-    // }
-    return data
-}
-
-paginate(octokit.search.issuesAndPullRequests)
-    .then(data => {
-        //console.log(data);
-        const usersAndCount = data.reduce((acc, pr) => {
-          const { user: { login } } = pr;
-          //console.log(login)
-          if (login in acc) {
-            acc[login]++; 
-          } else {
-            acc[login] = 1;
-          }
-          return acc;
-        }, {});
-
-        console.log(usersAndCount)
-    })
+getPRs(oktokit, org, sinceDate).then((data) => {
+  const tabDelimited = Object.keys(data).reduce(
+    (acc, year) =>
+      acc.concat(
+        Object.keys(data[year]).map(
+          (login) => `${year}\t${login}\t${data[year][login]}`
+        )
+      ),
+    []
+  );
+  tabDelimited.sort();
+  tabDelimited.forEach((line) => console.log(line));
+});
